09f163ffed14e30e6cb75b34ca4c4613b9830e0d
Brian He <brianhe@Brians-MacBook-Pro.local>
2026-03-03 21:42:57 -0500

    add random quiz and score tracking

--- Main.java
+++ Main.java
@@ -1,5 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nihongo Quiz App");
+        
+        ArrayList<Word> deck = new ArrayList<>(); 
+        deck.add(new Word("いぬ","dog"));
+        deck.add(new Word("ねこ","cat"));
+        deck.add(new Word("ぞう","elephant"));
+        deck.add(new Word("とり","bird"));
+        deck.add(new Word("みず","water"));
+        deck.add(new Word("きれい","beautiful"));
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int total = 5;
+        int correct = 0;
+
+        for (int i = 1; i <= total; i++){
+            Word w = deck.get(rand.nextInt(deck.size()));
+            System.out.println();
+            System.out.println("Question " + i + " / " + total);
+            System.out.println("Japanese: " + w.japanese);
+            System.out.println("Meaning in English: ");
+
+            String ans = sc.nextLine().trim();
+
+            if(ans.equalsIgnoreCase(w.english)){
+                System.out.println("Correct!");
+                correct++;
+            } else {
+                System.out.println("Wrong Answer, Right answer is " + w.english);
+            }
+        }
+        System.out.println();
+        System.out.println("Score: " + correct + " / " + total);
+
+        sc.close();
+
     }
 }
---
+++ Word.java
@@ -0,0 +1,11 @@
+public class Word {
+    //build 2 attributes for class Word
+    String japanese;
+    String english;
+
+    // constructor 
+    public Word(String j, String e){
+        japanese = j;
+        english = e;
+    }
+}
